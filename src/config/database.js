f42f665060c6442d6d95e05cad2a0d0c4cd19688
bruno <bruno-schmidt@users.noreply.github.com>
2019-02-12 17:54:30 -0200

    Remove config/database.js

--- src/config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  development: { username: 'salamander', password: 'salamander', database: 'salamanderdev', host: 'localhost', dialect: 'postgres' },
-  test: { username: 'salamander', password: 'salamander', database: 'salamandertest', host: 'localhost', dialect: 'postgres' },
-  production: { username: 'salamander', password: 'salamander', database: 'salamanderdev', host: 'localhost', dialect: 'postgres' }
-}
